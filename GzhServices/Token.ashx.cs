26786191f3e210286be9e07e16d86cc9291cbec4
cc <chenchi@qq.com>
2017-07-28 10:17:53 +0800

    cc commit

---
+++ GzhServices/Token.ashx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GzhServices
+{
+    /// <summary>
+    /// Token 的摘要说明
+    /// </summary>
+    public class Token : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+
+            string signature = context.Request.QueryString["signature"] ?? "";
+
+            string token = context.Request.QueryString["token"] ?? "";
+            string timestamp = context.Request.QueryString["timestamp"] ?? "";
+            string nonce = context.Request.QueryString["nonce"] ?? "";
+
+            string echostr = context.Request.QueryString["echostr"] ?? "";
+
+
+            /*System.Collections.Specialized.NameValueCollection collect = new System.Collections.Specialized.NameValueCollection();
+            collect.Add("Action", "GetCdnLogList");
+            string urlGet = "cdn.api.qcloud.com/v2/index.php?";
+            foreach (string key in collect.Keys)
+            {
+                urlGet += key + "=" + collect[key] + "&";
+            }
+            urlGet = urlGet.TrimEnd('&');
+            string SignatureGet = HttpUtility.UrlEncode(Util.HmacSha1Sign("Vjq0C8rJUUFo6FH4pa5RmrfDsUyGI5nh", "GET" + urlPost));*/
+
+            context.Response.Write(echostr);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ GzhServices/Util/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GzhServices
+{
+    public static class Util
+    {
+        /// <summary>
+        /// HMAC-SHA1加密算法
+        /// </summary>
+        /// <param name="secret">密钥</param>
+        /// <param name="strOrgData">源文</param>
+        /// <returns></returns>
+        public static string HmacSha1Sign(string secret, string strOrgData)
+        {
+            var hmacsha1 = new HMACSHA1(Encoding.UTF8.GetBytes(secret));
+            var dataBuffer = Encoding.UTF8.GetBytes(strOrgData);
+            var hashBytes = hmacsha1.ComputeHash(dataBuffer);
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
